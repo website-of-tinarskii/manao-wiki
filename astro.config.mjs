cf3f9550da35c32f56e329f462e3274f56f1316f
tinarskii <tinvv@outlook.co.th>
2026-02-22 03:37:57 +0700

    feat: add dynamic commands page and add new user guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,6 @@ export default defineConfig({
         },
       },
       customCss: [
-        // Fontsource files for to regular and semi-bold font weights.
         './src/styles/custom.css',
         '@fontsource/mitr/400.css',
         '@fontsource/mitr/600.css',
@@ -31,141 +30,150 @@ export default defineConfig({
       social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/tinarskii/manao'}],
       sidebar: [
         {
-          label: 'Guides',
-          translations: {
-            th: "คู่มือ"
-          },
+          label: 'Streamer Guide',
+          translations: { th: "คู่มือสำหรับสตรีมเมอร์" },
           items: [
             {
-              label: 'Getting Started',
-              translations: {
-                th: "มาเริ่มกันเถอะ"
-              },
-              slug: 'guides/00-getting-started'
+              label: 'Installation',
+              translations: { th: "การติดตั้ง" },
+              items: [
+                {
+                  label: 'Getting Started',
+                  translations: { th: "มาเริ่มกันเถอะ" },
+                  slug: 'guides/00-getting-started'
+                },
+                {
+                  label: 'Receive Client id and Secrets from Twitch',
+                  translations: { th: "รับไคล์แอนต์ไอดีและซีเครทจากทวิช" },
+                  slug: 'guides/01-receive-client-id-and-secrets-from-twitch'
+                },
+                {
+                  label: 'Project Installation',
+                  translations: { th: "ติดตั้งโปรเจกต์" },
+                  slug: 'guides/02-project-installation'
+                },
+                {
+                  label: 'Setting up Manao',
+                  translations: { th: "ตั้งค่ามะนาว" },
+                  slug: 'guides/03-setting-up-manao'
+                },
+              ]
             },
             {
-              label: 'Retrieving Client id and Secrets from Twitch',
-              translations: {
-                th: "รับไคล์แอนต์ไอดีและซีเครทจากทวิช"
-              },
-              slug: 'guides/01-receive-client-id-and-secrets-from-twitch'
+              label: 'Basic Usage',
+              translations: { th: "การใช้งานพื้นฐาน" },
+              items: [
+                {
+                  label: 'Using ManaoWeb',
+                  translations: { th: "เริ่มใช้งานมะนาวเว็บ" },
+                  slug: 'guides/04-using-manaoweb'
+                },
+                {
+                  label: 'Adding Overlays in OBS',
+                  translations: { th: "เพิ่มโอเวอร์เลย์ใน OBS" },
+                  slug: 'guides/05-adding-overlays-in-obs'
+                },
+              ]
             },
             {
-              label: 'Project Installation',
-              translations: {
-                th: "ติดตั้งโปรเจกต์"
-              },
-              slug: 'guides/02-project-installation'
+              label: 'Customization',
+              translations: { th: "การปรับแต่งขั้นสูง" },
+              items: [
+                {
+                  label: 'Linking Channel Points to Soundboard',
+                  translations: { th: "ลิงก์แต้มช่องกับซาวด์บอร์ด" },
+                  slug: 'guides/06-linking-channel-points-to-soundboard'
+                },
+                {
+                  label: 'Adding Custom Replies',
+                  translations: { th: "เพิ่มข้อความตอบกลับอัตโนมัติ" },
+                  slug: 'guides/07-adding-custom-replies'
+                },
+                {
+                  label: 'Writing your First Custom Command',
+                  translations: { th: "เขียนคำสั่งแรก" },
+                  slug: 'guides/08-writing-your-first-custom-command'
+                },
+              ]
             },
             {
-              label: 'Setting up Manao',
-              translations: {
-                th: "ตั้งค่ามะนาว"
-              },
-              slug: 'guides/03-setting-up-manao'
+              label: 'External Integrations',
+              translations: { th: "การเชื่อมต่อภายนอก" },
+              items: [
+                {
+                  label: 'Connecting to External Events',
+                  translations: { th: "เชื่อมต่อกับอีเวนต์ภายนอก" },
+                  slug: 'guides/09-connecting-to-external-events'
+                },
+              ]
             },
             {
-              label: 'Using ManaoWeb',
-              translations: {
-                th: "เริ่มใช้งานมะนาวเว็บ"
-              },
-              slug: 'guides/04-using-manaoweb'
+              label: 'Discord Integration',
+              translations: { th: "มะนาวกับ Discord" },
+              items: [
+                {
+                  label: 'Getting Started',
+                  translations: { th: "มาเริ่มกันเถอะ" },
+                  slug: 'discord/00-getting-started'
+                },
+                {
+                  label: 'Receive Discord Credentials',
+                  translations: { th: "รับโทเค็นและไอดีไคล์เอนต์ของบอต" },
+                  slug: 'discord/01-receive-discord-credentials'
+                },
+                {
+                  label: 'Making the Bot Online',
+                  translations: { th: "ทำให้บอตออนไลน์" },
+                  slug: 'discord/02-making-the-bot-online'
+                },
+              ]
             },
             {
-              label: 'Adding Overlays in OBS',
-              translations: {
-                th: "เพิ่มโอเวอร์เลย์ใน OBS"
-              },
-              slug: 'guides/05-adding-overlays-in-obs'
-            },
-            {
-              label: 'Linking Channel Points to Soundboard',
-              translations: {
-                th: "ลิงก์แต้มช่องกับซาวด์บอร์ด"
-              },
-              slug: 'guides/06-linking-channel-points-to-soundboard'
-            },
-            {
-              label: 'Adding Custom Replies',
-              translations: {
-                th: "เพิ่มข้อความตอบกลับอัตโนมัติ"
-              },
-              slug: 'guides/07-adding-custom-replies'
-            },
-            {
-              label: 'Writing your First Custom Command',
-              translations: {
-                th: "เขียนคำสั่งแรก"
-              },
-              slug: 'guides/08-writing-your-first-custom-command'
-            },
-            {
-              label: 'Connecting to External Events',
-              translations: {
-                th: "เชื่อมต่อกับอีเวนต์ภายนอก"
-              },
-              slug: 'guides/09-connecting-to-external-events'
+              label: 'Kick Integration',
+              translations: { th: "มะนาวกับ Kick" },
+              items: [
+                {
+                  label: 'Getting Started',
+                  translations: { th: "มาเริ่มกันเถอะ" },
+                  slug: 'kick/00-getting-started'
+                },
+                {
+                  label: 'Receive Kick Credentials',
+                  translations: { th: "รับข้อมูลของบอต" },
+                  slug: 'kick/01-receive-kick-credentials'
+                },
+                {
+                  label: 'Setup Ngrok',
+                  translations: { th: "ตั้งค่า Ngrok" },
+                  slug: 'kick/02-setup-ngrok'
+                },
+              ]
             },
           ],
         },
+
         {
-          label: 'Discord Integration',
-          translations: {
-            th: "มะนาวกับดิสคอร์ด"
-          },
+          label: 'User Guide',
+          translations: { th: "คู่มือสำหรับผู้ใช้" },
           items: [
             {
-            label: 'Getting Started',
-            translations: {
-              th: "มาเริ่มกันเถอะ"
-            },
-            slug: 'discord/00-getting-started'
-            },
-            {
-              label: "Receive Discord Credentials",
-              translations: {
-                th: "รับโทเค็นและไอดีไคล์เอนต์ของบอต"
-              },
-              slug: 'discord/01-receive-discord-credentials'
+              label: 'Commands',
+              translations: { th: 'คำสั่ง' },
+              items: [
+                {
+                  label: 'All Commands',
+                  translations: { th: 'คำสั่งทั้งหมด' },
+                  slug: 'commands'
+                },
+              ]
             },
             {
-              label: "Making the Bot Online",
-              translations: {
-                th: "ทำให้บอตออนไลน์"
-              },
-              slug: 'discord/02-making-the-bot-online'
-            }
+              label: 'Linking Your Account',
+              translations: { th: "การเชื่อมบัญชี" },
+              slug: 'user-guides/linking-your-account'
+            },
           ]
         },
-        {
-          label: 'Kick Integration',
-          translations: {
-            th: "มะนาวกับ Kick"
-          },
-          items: [
-            {
-              label: 'Getting Started',
-              translations: {
-                th: "มาเริ่มกันเถอะ"
-              },
-              slug: 'kick/00-getting-started'
-            },
-            {
-              label: "Receive Kick Credentials",
-              translations: {
-                th: "รับข้อมูลของบอต"
-              },
-              slug: 'kick/01-receive-kick-credentials'
-            },
-            {
-              label: "Setup Ngrok",
-              translations: {
-                th: "ตั้งค่า Ngrok"
-              },
-              slug: 'kick/02-setup-ngrok'
-            }
-          ]
-        }
       ],
     }),
   ],
